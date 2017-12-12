032773db4e7a21b636cdaebe11c8919dfa606c00
armitage81 <contact@anton-afanasjew.de>
2017-12-12 16:22:00 +0100

    Update Increments.java

--- examples/0000015.Increments/Increments.java
+++ examples/0000015.Increments/Increments.java
@@ -4,7 +4,7 @@ public class Increments {
 
 	public static void main(String[] args) {
 		int i=0;
-		
+		//This will run forever because i is increased only after the assignment of itself to the var i
 		for (int j = 0; j < 10; j++) {
 			i = i++;
 			System.out.println(i);
